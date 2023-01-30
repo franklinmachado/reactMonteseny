d27718bbaecc53fcce5e2ca1ab6b4434bfac7151
lucasalmeida <lac.lucas96@gmail.com>
2023-01-30 18:13:35 -0300

    cria rotas e adiciona estilos base

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
-function App() {
-	return <h1>Hello World</h1>;
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root";
+import { Home } from "./pages/Home";
+import { Sobre } from "./pages/Sobre";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/sobre",
+        element: <Sobre />
+      },
+      {
+        path: "/reserva",
+      },
+      {
+        path: "/acomodacoes"
+      },
+      {
+        path: "/cadastro"
+      },
+      {
+        path: "/login"
+      },
+      {
+        path: "/contato"
+      }
+    ]
+  }
+])
+
+export default function App() {
+	return (
+    <RouterProvider router={router} />
+  );
+}
